10eb13c9b75e1e4496849548e1e3ead44e812652
jimemena91 <jimemenarodriguez91@gmail.com>
2026-06-25 03:12:58 +0000

    feat(queue): add manual daily closing endpoint

--- backend/src/modules/daily-closing/daily-closing.module.ts
+++ backend/src/modules/daily-closing/daily-closing.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 
+import { DailyClosingController } from './daily-closing.controller';
+
 import { DailyClosingRepository } from './daily-closing.repository';
 import { DailyClosingService } from './daily-closing.service';
 
 @Module({
+  controllers: [DailyClosingController],
   providers: [DailyClosingRepository, DailyClosingService],
   exports: [DailyClosingService],
 })
